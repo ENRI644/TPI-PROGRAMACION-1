5478a21f4cb495ed3960e7eadeb9de8f05cebb8a
enri644 <cre_comercial-094@hotmail.com>
2026-06-27 17:55:46 -0300

    primera verion

---
+++ TPI version 0.1.py
@@ -0,0 +1,30 @@
+# La empresa Play.In desea desarrollar una plataforma educativa destinada a estudiantes de
+# nivel primario que permita aprender mediante actividades interactivas.
+# Para ello solicita a los equipos de desarrollo la construcción de una aplicación que integre
+# distintos juegos o herramientas didácticas dentro de un único sistema.
+# El objetivo es que el usuario pueda elegir libremente qué actividad utilizar y regresar al menú
+# principal tantas veces como desee.
+# Cada integrante del equipo deberá ser responsable principal del desarrollo de al menos una
+# actividad, aunque todo el grupo será responsable del funcionamiento integral del sistema.
+# Ejemplo
+# ****************************************************
+# BIENVENIDOS A 4IN1
+# ****************************************************
+# 1- Ahorcado
+# 2- Adivina la palabra
+# 3- A Multiplicar
+# 4- ¿Cuánto conoces los números?
+# 0- Salir
+# Ingresa tu opción:
+# *****************************************************
+# (si ingreso 4)
+# ****************************************************
+# 4-¿Cuánto conoces los números?
+# ****************************************************
+# ¿El número 3 es un número primo?
+# 1- Si
+# 2- No
+# 3- Volver al menú sin contestar
+# Ingresa tu opción:
+# *****************************************************
+
